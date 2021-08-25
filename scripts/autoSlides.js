68d2a2e862f0a8a5663087b0dd5bf079bed0ca9a
Tim Robbins <GroggyAlgorithm@gmail.com>
2021-08-24 21:12:25 -0400

    Add files via upload

---
+++ scripts/autoSlides.js
@@ -0,0 +1,28 @@
+// JavaScript source code
+"use strict";
+var autoSlideIndex = 0;
+AutoSlides(autoSlideIndex);
+
+
+
+function AutoSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("auto-slides");
+    var slideLength = slides.length;
+
+
+    for (i = 0; i < slideLength; i++) {
+        slides[i].style.display = "none";
+    }
+
+
+
+    autoSlideIndex++;
+
+
+    if (autoSlideIndex > slideLength) { autoSlideIndex = 1; }
+
+    slides[autoSlideIndex - 1].style.display = "block";
+
+    setTimeout(AutoSlides, 5000); // time out for passed # of seconds
+}
--- scripts/slides.js
+++ scripts/slides.js
@@ -3,27 +3,58 @@
 "use strict";
 
 
-
-var slideBox = document.getElementById("slideBox"); 
-
-var btnLeft = document.getElementById("btnRight");
-
-var Right = document.getElementById("btnLeft");
+var slideIndex = 1;
+ShowSlides(slideIndex);
 
 
+// Next/previous controls
+function plusSlides(n) {
+    
 
 
-
-function jump() {
-	//If the player does not have the class on it..
-	if (slideBox.classList != "animate-slide") {
-		slideBox.classList.add("animate-slide"); //add the slide class from the css
-	}
-	setTimeout(function () { //Then, we declate set timeout function and that removes the animation 
-		//After....
-		slideBox.classList.remove("animate-slide");
-	}, 800) //runs every 500ms
+    ShowSlides(slideIndex += n);
+    
 }
+
+// Thumbnail image controls
+function currentSlide(n) {
+    
+    ShowSlides(slideIndex = n);
+}
+
+
+
+function ShowSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var slideLength = slides.length;
+    var dots = document.getElementsByClassName("slide-dots");
+
+    if (n > slideLength) { slideIndex = 1; }
+
+    if (n < 1) { slideIndex = slideLength; }
+
+    for (i = 0; i < slideLength; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
